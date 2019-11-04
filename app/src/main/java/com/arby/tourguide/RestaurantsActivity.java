f19e3a3151bdabbe49c849d083313a5b1c52316d
Rares Borcea <raresborcea@gmail.com>
2019-11-04 21:34:29 +0200

    Update RestaurantsActivity.java

--- app/src/main/java/com/arby/tourguide/RestaurantsActivity.java
+++ app/src/main/java/com/arby/tourguide/RestaurantsActivity.java
@@ -27,17 +27,10 @@ public class RestaurantsActivity extends AppCompatActivity {
         words.add(new Word(getString(R.string.rname5), getString(R.string.radd5), getString(R.string.rhour5)));
         words.add(new Word(getString(R.string.rname6), getString(R.string.radd6), getString(R.string.rhour6)));
 
-        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
-        // adapter knows how to create list items for each item in the list.
+        // Create an WordAdapter, whose data source is a list of Words
         WordAdapter adapter = new WordAdapter(this, words, R.color.category_restaurants);
 
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // word_list.xml layout file.
         ListView listView = (ListView) findViewById(R.id.list);
-
-        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
-        // {@link ListView} will display list items for each {@link Word} in the list.
         listView.setAdapter(adapter);
     }
 }
