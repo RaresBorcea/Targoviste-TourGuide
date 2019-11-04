9b7c3938b1f749f8298f1491056545a0c196cd91
Rares Borcea <raresborcea@gmail.com>
2019-11-04 21:31:40 +0200

    Update MainActivity.java

--- app/src/main/java/com/arby/tourguide/MainActivity.java
+++ app/src/main/java/com/arby/tourguide/MainActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.arby.tourguide;
 
 import android.content.Intent;
@@ -27,12 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
         TextView restaurants = (TextView) findViewById(R.id.restaurants);
 
-        // Set a click listener on that View
         restaurants.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
         TextView museums = (TextView) findViewById(R.id.museums);
 
-        // Set a click listener on that View
         museums.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
 
         TextView pubs = (TextView) findViewById(R.id.pubs);
 
-        // Set a click listener on that View
         pubs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
 
         TextView parks = (TextView) findViewById(R.id.parks);
 
-        // Set a click listener on that View
         parks.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
