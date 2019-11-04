914e6b639e45d21fc8f875dae02f998f920431f9
Rares Borcea <raresborcea@gmail.com>
2019-11-04 21:33:55 +0200

    Update PubsActivity.java

--- app/src/main/java/com/arby/tourguide/PubsActivity.java
+++ app/src/main/java/com/arby/tourguide/PubsActivity.java
@@ -26,17 +26,10 @@ public class PubsActivity extends AppCompatActivity {
         words.add(new Word(getString(R.string.pname5), getString(R.string.padd5), getString(R.string.phour5)));
         words.add(new Word(getString(R.string.pname6), getString(R.string.padd6), getString(R.string.phour6)));
 
-        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
-        // adapter knows how to create list items for each item in the list.
+        // Create an WordAdapter, whose data source is a list of Words
         WordAdapter adapter = new WordAdapter(this, words, R.color.category_pubs);
 
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // word_list.xml layout file.
         ListView listView = (ListView) findViewById(R.id.list);
-
-        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
-        // {@link ListView} will display list items for each {@link Word} in the list.
         listView.setAdapter(adapter);
     }
 }
